573aadaee6ca60dbfd75fbff8828a28319f3c53e
John Doe <atarod.shirin@gmail.com>
2022-10-13 14:35:55 +0200

    iteration2 done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 const background = new Image();
 background.src = "../images/road.png";
+
+const car = new image();
+car.src = "../images/car.png"
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
