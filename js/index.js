7530efb3547109ba63884df87c25ca010cfc272b
John Doe <atarod.shirin@gmail.com>
2022-10-13 23:34:59 +0200

    iteration4 edited

--- js/index.js
+++ js/index.js
@@ -7,47 +7,70 @@ background.src = "../images/road.png";
 const car = new Image();
 car.src = "../images/car.png";
 
-let backgroundY = 0;
-let backgroundY2 = 0;
-let carx = 210;
 
-//backgroundY += 4;
-//backgroundY2 += 4;
+let background1Y = 0;
+let background2Y = 0;
+let carX = 210, carY = 500;
+let obsX = 60, obsY = -60;
 
-function clearCtx() {
+//how the game works
+
+let intervalId;
+
+function startGame() {
+  if ( !intervalId ) {
+    obsX = 60, obsY = -60;
+    scoreElement.innerText = "";
+    intervalId = setInterval(() => {
+      handler();
+    }, 50);
+  }
+};
+
+scoreElement = document.getElementById("score");
+let score = parseInt( scoreElement.innerText );
+
+function handler() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(background, 0, backgroundY, canvas.width, canvas.height);
+  ctx.drawImage(background, 0, background1Y, canvas.width, canvas.height);
+  ctx.drawImage(background, 0, background2Y, canvas.width, canvas.height);
+  ctx.fillRect(obsX, obsY, 120, 40);
+  ctx.drawImage(car, carX, carY, 70, 120);
+
+  obsY += 20;
+  if (obsY >= canvas.height) {
+    obsY = -40;
+    obsX = randomIntFromInterval(60, canvas.width - 160);
+  }
+
+  if (background2Y >= 50) {
+    background2Y = 0;
+  }
+  background2Y += 10;
+  
+  if ( ( carX >= obsX - 35 && carX <= obsX + 120 ) && carY <= obsY ){
+    score = "Game Over";
+    scoreElement.innerText = score;
+    clearInterval(intervalId);
+    intervalId = 0;
+  }
 }
 
 function moveLeft() {
-  if (carx >= 80){
-    clearCtx();
-    carx -= 5;
-    ctx.drawImage(car, carx, 500, 70, 120);
+  if (carX >= 80) {
+    carX -= 20;
   };
 };
 
 function moveRight() {
-  if (carx<=canvas.width - 140) {
-  clearCtx();
-  ctx.drawImage(car, carx, 500, 70, 120);
-  carx += 5;
+  if (carX <= canvas.width - 140) {
+    carX += 20;
   }
 };
 
-function startGame() {
-  ctx.drawImage(background, 0, backgroundY, canvas.width, canvas.height);
-  ctx.drawImage(background, 0, backgroundY2, canvas.width, canvas.height);
-  ctx.drawImage(car, carx, 500, 70, 120);
-
-  // if (backgroundY > canvas.height) {
-  //   backgroundY = -canvas.height - 20;
-  // }
-  // if (backgroundY2 > canvas.height) {
-  //   backgroundY2 = -canvas.height;
-  // }
-
-};
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 
 document.addEventListener('keydown', Event => {
@@ -55,7 +78,7 @@ document.addEventListener('keydown', Event => {
     moveLeft();
   }
   if (Event.key === `ArrowRight`) {
-  moveRight();
+    moveRight();
   }
 });
 
