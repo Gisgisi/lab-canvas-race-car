e87d97ca30235fe5abc2578edd6b00d4455a561d
John Doe <atarod.shirin@gmail.com>
2022-10-13 15:46:24 +0200

    undone

--- js/index.js
+++ js/index.js
@@ -1,13 +1,35 @@
+const canvas = document.querySelector(`#canvas`);
+const ctx = canvas.getContext("2d");
+
 const background = new Image();
 background.src = "../images/road.png";
 
-const car = new image();
-car.src = "../images/car.png"
+const car = new Image();
+car.src = "../images/car.png";
+
+let backgroundY = 0;
+let backgroundY2 = 0;
+
+backgroundY += 4
+backgroundY2 += 4
 
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
+    console.log("start-button")
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(background, 0, backgroundY, canvas.width, canvas.height);
+    ctx.drawImage(background, 0, backgroundY2, canvas.width, canvas.height);
+    ctx.drawImage(car, 215, 500, 70, 120);
+
+    if (backgroundY > canvas.height) { 
+      backgroundY = -canvas.height - 20;
+    }
+    if (backgroundY2 > canvas.height){
+      backgroundY2 = -canvas.height;
+    }
+    setTimeout(startGame, 200)
+  }
 };
