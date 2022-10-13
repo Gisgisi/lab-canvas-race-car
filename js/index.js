0a7bbe25c1670772c6a7a67589bdc49eef91cd33
John Doe <atarod.shirin@gmail.com>
2022-10-13 17:46:14 +0200

    iteration3 done

--- js/index.js
+++ js/index.js
@@ -9,27 +9,60 @@ car.src = "../images/car.png";
 
 let backgroundY = 0;
 let backgroundY2 = 0;
+let carx = 210;
 
-backgroundY += 4
-backgroundY2 += 4
+//backgroundY += 4;
+//backgroundY2 += 4;
+
+function clearCtx() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(background, 0, backgroundY, canvas.width, canvas.height);
+}
+
+function moveLeft() {
+  if (carx >= 80){
+    clearCtx();
+    carx -= 5;
+    ctx.drawImage(car, carx, 500, 70, 120);
+  };
+};
+
+function moveRight() {
+  if (carx<=canvas.width - 140) {
+  clearCtx();
+  ctx.drawImage(car, carx, 500, 70, 120);
+  carx += 5;
+  }
+};
+
+function startGame() {
+  ctx.drawImage(background, 0, backgroundY, canvas.width, canvas.height);
+  ctx.drawImage(background, 0, backgroundY2, canvas.width, canvas.height);
+  ctx.drawImage(car, carx, 500, 70, 120);
+
+  // if (backgroundY > canvas.height) {
+  //   backgroundY = -canvas.height - 20;
+  // }
+  // if (backgroundY2 > canvas.height) {
+  //   backgroundY2 = -canvas.height;
+  // }
+
+};
+
+
+document.addEventListener('keydown', Event => {
+  if (Event.key === 'ArrowLeft') {
+    moveLeft();
+  }
+  if (Event.key === `ArrowRight`) {
+  moveRight();
+  }
+});
 
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     console.log("start-button")
     startGame();
   };
+}
 
-  function startGame() {
-    ctx.drawImage(background, 0, backgroundY, canvas.width, canvas.height);
-    ctx.drawImage(background, 0, backgroundY2, canvas.width, canvas.height);
-    ctx.drawImage(car, 215, 500, 70, 120);
-
-    if (backgroundY > canvas.height) { 
-      backgroundY = -canvas.height - 20;
-    }
-    if (backgroundY2 > canvas.height){
-      backgroundY2 = -canvas.height;
-    }
-    setTimeout(startGame, 200)
-  }
-};
